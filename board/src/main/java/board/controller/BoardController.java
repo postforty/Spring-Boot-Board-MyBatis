bdf5848a4676a864ce3ad92fc75c5931fb1d1815
postforty <75767073+postforty@users.noreply.github.com>
2022-08-31 17:03:46 +0900

    [ADD] service

--- board/src/main/java/board/controller/BoardController.java
+++ board/src/main/java/board/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import board.dto.BoardDto;
+import board.service.BoardService;
 
 @Controller
 public class BoardController {
@@ -16,7 +17,7 @@ public class BoardController {
 	private BoardService boardService;
 	
 	@RequestMapping("/board/openBoardList.do")
-	public ModelAndView openBoardList() {
+	public ModelAndView openBoardList() throws Exception {
 		ModelAndView mv = new ModelAndView("/board/boardList");
 		
 		List<BoardDto> list = boardService.selectBoardList();
---
+++ board/src/main/java/board/service/BoardService.java
@@ -0,0 +1,11 @@
+package board.service;
+
+import java.util.List;
+
+import board.dto.BoardDto;
+
+public interface BoardService {
+	
+	List<BoardDto> selectBoardList() throws Exception;
+
+}
