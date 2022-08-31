cd1b66324943bfb5b62c1a088bb9ac50c1be4957
postforty <75767073+postforty@users.noreply.github.com>
2022-08-31 18:49:30 +0900

    [ADD] mapper

--- board/src/main/java/board/service/BoardServiceImpl.java
+++ board/src/main/java/board/service/BoardServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import board.dto.BoardDto;
+import board.mapper.BoardMapper;
 
 @Service
 public class BoardServiceImpl implements BoardService {
